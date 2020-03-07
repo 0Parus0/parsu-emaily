758a4c7cd6e64824f49f15a216a1c33aab54121a
Parus <0Parus0@gmail.com>
2020-03-07 10:55:19 +0500

    Finished authentication with google

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+
+require('./models/User');
+require('./services/passport');
+const keys = require('./config/keys');
+
+
+
+mongoose.connect(keys.mongoUri , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected');
+})
+
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
 
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 3000
+
 
 
 app.listen(PORT, () => {
   console.log('Listening on port 3000');
 })
+
+
---
+++ mongoose.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://localhost/parus-emaily', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected');
+})
+
+const User = mongoose.model('users', {
+  name: {
+    type: String
+  }, 
+  age: {
+    type: Number
+  }
+});
+
+
+const me = new User({
+  name: 'Parus',
+  age: 39
+});
+
+me.save()
+  .then(() => {
+    console.log(me)
+  })
+  .catch((error) => {
+    console.log('Error: ', error)
+  })
